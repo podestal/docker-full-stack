aa6ba417caf0eff17c1279001b5b6c729e0a44cc
podestal <luisrodriguezpodesta@gmail.com>
2024-01-16 13:21:51 -0500

    backend set up

--- backend/docker_backend/settings.py
+++ backend/docker_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_fraemwork',
+    'rest_framework',
     'core',
 ]
 
@@ -129,6 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
