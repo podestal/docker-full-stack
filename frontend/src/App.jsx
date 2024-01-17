040d15ea3277aff582a7a60960ac3112a0afe80b
podestal <luisrodriguezpodesta@gmail.com>
2024-01-17 16:59:29 -0500

    add todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,52 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
-
+import { useQuery, useMutation } from "@tanstack/react-query"
 
 const App = () => { 
 
-  const [todos, setTodos] = useState([])
+  const [title, setTitle] = useState()
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/todos/')
-    .then(res => setTodos(res.data))
-    .catch(err => console.log(err))
-  }, [])
+  const {mutate} = useMutation({
+    mutationFn: (todos) => {
+      return axios.post('http://127.0.0.1:8000/api/todos/', todos )
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+    }
+  })
+
+  const { data: todos, isLoading, isError, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () => {
+      return axios.get('http://127.0.0.1:8000/api/todos/')
+      .then(res => res)
+      .catch(err => console.log(err))
+    }
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    mutate({ title })
+  }
+
+  if (isLoading) return <p>Loading ...</p>
+
+  if (isError) return <p>{error.message}</p>
 
   return (
-    <>
+    <div>
       <h1>Todo App</h1>
-      {todos && todos.map(todo => <p key={todo.id}>{todo.title}</p>)}
-    </>
+      {todos.data.map(todo => <p key={todo.id}>{todo.title}</p>)}
+      {console.log(todos.data)}
+      <form>
+        <input 
+          type="text" 
+          placeholder="New Todo"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
+    </div>
   )
 }
 
