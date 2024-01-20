59e98b9b6b66dac2a5906faa30ab546ac019634a
podestal <luisrodriguezpodesta@gmail.com>
2024-01-19 21:00:42 -0500

    serializing

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -5,3 +5,8 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Todo
         fields = '__all__'
+
+class CreateTodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Todo
+        fields = ['title']
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -5,4 +5,8 @@ from . import serializers
 
 class TodoViewSet(ModelViewSet):
     queryset = models.Todo.objects.all()
-    serializer_class = serializers.TodoSerializer
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.CreateTodoSerializer
+        return serializers.TodoSerializer
